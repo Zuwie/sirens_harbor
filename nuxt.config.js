3b51323cf34bc6ca1c0edc437b9142d092ef3e80
rafael.seifert <rafael.seifert@prescreen.io>
2020-09-10 07:28:07 +0200

    Adjust base config and change to new space-token

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
     [
       'storyblok-nuxt',
       {
-        accessToken: 'mNdD4LVMf6YeIVLXcY4H4gtt',
+        accessToken: 'bSL4ZACqoNLhsmfv8rnR3Qtt',
         cacheProvider: 'memory',
       },
     ],
@@ -76,20 +76,6 @@ export default {
    */
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
   },
   /*
    ** Build configuration
