d8024462d8e681de5117a4f1cc482740439bbf75
rafael.seifert <rafael.seifert@prescreen.io>
2020-09-18 16:47:01 +0200

    remove music in favor of albums

--- plugins/components.js
+++ plugins/components.js
@@ -2,11 +2,9 @@ import Vue from 'vue'
 import Page from '@/components/Page'
 import About from '@/components/About'
 import Team from '@/components/Team'
-import Music from '@/components/Music'
 import Albums from '@/components/Albums'
 
 Vue.component('page', Page)
 Vue.component('about', About)
 Vue.component('team', Team)
-Vue.component('music', Music)
 Vue.component('albums', Albums)
