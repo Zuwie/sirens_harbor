27f9c6b7a3338fa311de3198f9601deb12c5424a
rafael.seifert <rafael.seifert@prescreen.io>
2020-09-18 16:52:29 +0200

    add album component and make use of it

---
+++ components/Album.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Album</div>
+</template>
+
+<script>
+export default {
+  name: 'Album',
+}
+</script>
--- components/Albums.vue
+++ components/Albums.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>{{ blok }}</div>
+  <div>
+    <v-container>
+      <h2 class="text-center text-h2 mb-10">{{ blok.headline }}</h2>
+      <v-row>
+        <album
+          v-for="album in blok.band"
+          :key="album._uid"
+          :headline="album.name"
+          :body="album.body"
+          :image="album.image"
+        ></album>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import Album from '@/components/Album'
+
 export default {
+  components: { Album },
   props: ['blok'],
 }
 </script>
